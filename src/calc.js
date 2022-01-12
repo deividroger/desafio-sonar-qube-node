4c9028a5a0734ced195853a7b0c7fc7f7bc751b9
Deivid Roger Oliveira Santos <deivid@deividroger.net>
2022-01-12 00:56:11 -0300

    first commit

---
+++ src/calc.js
@@ -0,0 +1,11 @@
+function sum(a,b) {
+    return a +b;
+}
+
+function times(a,b) {
+    return a  * b;
+}
+
+
+exports.sum = sum;
+exports.times = times;
---
+++ src/calc.test.js
@@ -0,0 +1,12 @@
+
+const calc = require('./calc')
+
+test("add 1 + 2 to be equal 3", ()=>{
+
+    expect(calc.sum(1,2)).toBe(3);
+});
+
+test("2 * 2 to be equal 4", ()=>{
+
+    expect(calc.times(2,2)).toBe(4);
+});
